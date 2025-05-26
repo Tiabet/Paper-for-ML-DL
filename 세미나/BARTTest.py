88780bb299a44c6b0c857f983571e98e0843a708
Tiabet <kkksk1157@naver.com>
2025-05-26 16:57:08 +0900

    update

--- 세미나/BARTTest.py
+++
@@ -1,9 +0,0 @@
-from transformers import BartModel, BartTokenizer, BartConfig
-
-
-model = BartModel.from_pretrained('facebook/bart-large')
-# tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
-
-
-# configuration = BartConfig()
-print(model)
